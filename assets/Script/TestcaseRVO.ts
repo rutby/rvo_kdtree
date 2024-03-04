f8ab5968cce099ee1e38bb0eac3de98baded9e9d
chentao <zhonghang181@163.com>
2024-03-04 18:25:01 +0800

    t

--- assets/Script/TestcaseRVO.ts
+++ assets/Script/TestcaseRVO.ts
@@ -1,6 +1,13 @@
 import { RVOMath, Vector2 } from "./RVO/common";
 import { Simulator } from "./RVO/simulator";
 
+interface IAgent {
+    id?: number,
+    pos: Vector2;
+    dst: Vector2;
+    volocity: Vector2;
+}
+
 const {ccclass, property} = cc._decorator;
 
 @ccclass
@@ -10,105 +17,163 @@ export default class TestcaseRVO extends cc.Component {
     @property(cc.Float) public radius: number = 20;
     @property(cc.Float) public num: number = 9;
 
+    private _agents_d: IAgent[] = [];
+    private _agents_n: cc.Node[] = [];
     protected start(): void {
         let simulator = new Simulator();
         Simulator.instance = simulator;
 		this.setup(simulator);
 
-        this.schedule(() => {
-		    this.setPreferredVelocities(simulator);
-			simulator.run();
-            if (simulator.reachedGoal()) {
-                console.log('[develop] ========', 'reachedGoal');
-                this.unscheduleAllCallbacks();
-            }
-        }, 0);
+        /** touch */
+        this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchStart, this);
+		this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+		this.node._touchListener.setSwallowTouches(false);
+		this.node.setContentSize(cc.winSize);
     }
 
-    private _agents: cc.Node[] = [];
+    
     protected update(dt: number): void {
         if (!Simulator.instance) {
             return;
         }
+        let simulator = Simulator.instance;
 
-        var numAgents = Simulator.instance.getNumAgents();
-				
-        for (var i=0; i<numAgents; i++) {
-            if (!this._agents[i]) {
-                let node = cc.instantiate(this.nodeAgent);
-                node.parent = this.node;
-                this._agents[i] = node;
+        /** 重置pref-volocity */
+        for (var i = 0; i < this._agents_d.length; i++) {
+            let agent_data = this._agents_d[i];
+            let aid = agent_data.id;
+            let pos0 = simulator.getAgentPosition(aid);
+            let pos1 = agent_data.dst;
+
+            let volocity = null;
+            if (RVOMath.absSq(pos0.minus(pos1)) < RVOMath.RVO_EPSILON) {
+                volocity = new Vector2(0, 0);
+            } else {
+                volocity = RVOMath.normalize(pos1.minus(pos0));
             }
+            simulator.setAgentPrefVelocity(aid, volocity);
 
-            var pos = Simulator.instance.getAgentPosition(i);
-            var radius = Simulator.instance.getAgentRadius(i);
-            this._agents[i].x = pos.x;
-            this._agents[i].y = pos.y;
+            agent_data.pos = pos0;
+            agent_data.volocity = volocity;
+        }
+
+        /** update logic */
+        simulator.run();
+
+        /** update ui */
+        for (var i = 0; i < this._agents_d.length; i++) {
+            let agent_data = this._agents_d[i];
+            var pos = Simulator.instance.getAgentPosition(agent_data.id);
+            var radius = Simulator.instance.getAgentRadius(agent_data.id);
+
+            let agent_node = this._agents_n[i];
+            agent_node.x = pos.x;
+            agent_node.y = pos.y;
         }
     }
+
+    //================================================ touch
+    private _touchValid: boolean = false;
+	private onTouchStart(event: cc.Event.EventTouch): void {
+		this._touchValid = true;
+	}
+
+	private onTouchEnd(event: cc.Event.EventTouch): void {
+		if (!this._touchValid) {
+			return;
+		}
+		this._touchValid = false;
+
+		console.log('[develop] ========', 'hah');
+	}
+
 
     //================================================ private
-    private setPreferredVelocities(simulator) {
-        var stopped = 0;
-        for (var i = 0; i < simulator.getNumAgents (); ++i) {
-            if (RVOMath.absSq(simulator.getGoal(i).minus(simulator.getAgentPosition(i))) < RVOMath.RVO_EPSILON) {
-                // Agent is within one radius of its goal, set preferred velocity to zero
-                simulator.setAgentPrefVelocity (i, new Vector2 (0.0, 0.0));
-                stopped++;
-            } else {
-                // Agent is far away from its goal, set preferred velocity as unit vector towards agent's goal.
-                simulator.setAgentPrefVelocity(i, RVOMath.normalize (simulator.getGoal(i).minus(simulator.getAgentPosition(i))));
-            }
-        }
-        return stopped;
-    }
-
-    private setup(simulator): void {
-        // Specify global time step of the simulation.
+    private setup(simulator: Simulator): void {
         simulator.setTimeStep(this.speed);
-
-        // Specify default parameters for agents that are subsequently added.
-        var velocity = new Vector2(1, 1);
-        var radius = this.radius;
         simulator.setAgentDefaults(
-                400, // neighbor distance (min = radius * radius)
-                30, // max neighbors
-                600, // time horizon
-                600, // time horizon obstacles
-                radius, // agent radius
-                10.0, // max speed
-                velocity // default velocity
-            );
-        var NUM_AGENTS = this.num;
-        for (var i=0; i<NUM_AGENTS; i++) {
-            var angle = i * (2*Math.PI) / NUM_AGENTS;
+            400,                // neighbor distance (min = radius * radius)
+            30,                 // max neighbors
+            600,                // time horizon
+            600,                // time horizon obstacles
+            this.radius,        // agent radius
+            10.0,               // max speed
+            new Vector2(1, 1)   // default velocity
+        );
+
+        /** 生成agent */
+        this.node.destroyAllChildren();
+        this._agents_d = [];
+        this._agents_n = [];
+        var numAgents = this.num;
+        for (var i = 0; i < numAgents; i++) {
+            var angle = i * (2 * Math.PI) / numAgents;
             var x = Math.cos(angle) * 200;
             var y = Math.sin(angle) * 200;
-            simulator.addAgent(new Vector2 (x,y));
+            let pos = new Vector2(x,y);
+
+            let pos0 = pos;
+            let pos1 = pos.scale(-1);
+            let volocity = RVOMath.normalize(pos1.minus(pos0));
+
+            this.newAgent(pos0, pos1, volocity);
         }
 
-        // Create goals
-        var goals = [];
-        for (var i = 0; i < simulator.getNumAgents (); ++i) {
-            goals.push(simulator.getAgentPosition(i).scale(-1));
-        }
-        simulator.addGoals(goals);
-
-        // Add (polygonal) obstacle(s), specifying vertices in counterclockwise order.
-        // var vertices = [];
-
-        // if ($("obstacles").checked) {
-        //     for (var i=0; i<3; i++) {
-        //         var angle = i * (2*Math.PI) / 3;
-        //         var x = Math.cos(angle) * 50;
-        //         var y = Math.sin(angle) * 50;
-        //         vertices.push(new Vector2(x,y));
-        //     }
-        // }
-
-        // simulator.addObstacle (vertices);
-
-        // Process obstacles so that they are accounted for in the simulation.
         simulator.processObstacles();
     }
+
+    private newAgent(pos0: Vector2, pos1: Vector2, volocity: Vector2): void {
+        let simulator = Simulator.instance;
+
+        let agent_data: IAgent = {
+            pos: pos0,
+            dst: pos1,
+            volocity: volocity,
+        };
+        this._agents_d.push(agent_data);
+
+        let node = cc.instantiate(this.nodeAgent);
+        node.parent = this.node;
+        this._agents_n.push(node);
+
+        let aid = simulator.addAgent(agent_data.pos);
+        simulator.setAgentPrefVelocity(aid, agent_data.volocity);
+        agent_data.id = aid;
+    }
 }
+
+// //================================================ private
+// 	/** angle a -> b */
+// 	private static _stepAngle(a: number, b: number, c: number): number {
+// 		a = this._0To360(a);
+// 		b = this._0To360(b);
+
+// 		let dir1 = this._isCCW(a, b) ? 1 : -1;
+// 		let d = this._0To360(a + c * dir1);
+// 		let dir2 = this._isCCW(d, b) ? 1 : -1;
+// 		if (dir1 !== dir2) {
+// 			d = b;
+// 		}
+
+// 		d = this._180To180(d);
+// 		return d;
+// 	}
+
+// 	/** 0~360 */
+// 	private static _0To360(angle: number): number {
+// 		angle = angle % 360;
+// 		return angle < 0 ? angle + 360 : angle;
+// 	}
+
+// 	/** -180~180 */
+// 	private static _180To180(angle: number): number {
+// 		return angle > 180 ? angle - 360 : angle;
+// 	}
+
+// 	/** ccw */
+// 	private static _isCCW(from: number, to: number): boolean {
+// 		let diff = to - from;
+//         let cond1 = 180 > diff && diff > 0;
+//         let cond2 = -180 > diff && diff < 0;
+//         return cond1 || cond2;
+// 	}
